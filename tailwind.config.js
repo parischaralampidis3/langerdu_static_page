45bc7c87490a66f149262cb7e18e2fd581935145
Paris Charalampidis <parou.char@gmail.com>
2026-02-15 21:30:53 +0200

    initiate tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",      // any HTML in the root
+    "./src/**/*.css", // optional, if you use @apply
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
